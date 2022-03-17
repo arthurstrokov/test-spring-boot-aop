78511883cbe9ec78d53731f55cc90a083613df8e
Arthur Strokov <arthurstrokov@gmail.com>
2022-03-18 00:53:00 +0300

    first commit

---
+++ src/main/java/com/gmail/arthurstrokov/aop/TestAopApplication.java
@@ -0,0 +1,26 @@
+package com.gmail.arthurstrokov.aop;
+
+import com.gmail.arthurstrokov.aop.service.BeanService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TestAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAopApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return args -> {
+
+            BeanService beanService = (BeanService) context.getBean("beanService");
+            beanService.doSomeJob();
+            beanService.getBeans(context);
+        };
+    }
+}
---
+++ src/main/java/com/gmail/arthurstrokov/aop/service/BeanService.java
@@ -0,0 +1,30 @@
+package com.gmail.arthurstrokov.aop.service;
+
+import com.gmail.arthurstrokov.aop.LogExecutionTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class BeanService {
+
+    public static final Logger logger = LoggerFactory.getLogger(BeanService.class);
+
+    @LogExecutionTime
+    public void doSomeJob() throws InterruptedException {
+        logger.info("Doing some job");
+        Thread.sleep(1000);
+        logger.info("Stop doing job");
+    }
+
+    @LogExecutionTime
+    public void getBeans(ApplicationContext context) {
+        logger.info("Let's inspect the beans provided by Spring Boot:");
+        String[] beanNames = context.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        Arrays.stream(beanNames).forEach(logger::info);
+    }
+}
